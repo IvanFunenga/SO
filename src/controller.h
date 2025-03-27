56e1b2143eb9dfc0e908c4180657bc9f7d76fcb5
IvanFunenga <152197447+IvanFunenga@users.noreply.github.com>
2025-03-27 17:26:11 +0000

    Delete src directory

--- src/controller.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
-
-#include "common.h"
-
-void print_config(const char* config_file);
-
-#endif // CONTROLLER_H
--- src/ipc_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef IPC_UTILS_H
-#define IPC_UTILS_H
-
-#include "common.h"
-
-void initialize_ipc();
-void cleanup_ipc();
-
-#endif // IPC_UTILS_H
