e3832d44387a84977d349056761cd22529e927a9
IvanFunenga <152197447+IvanFunenga@users.noreply.github.com>
2025-03-29 03:58:12 +0000

    Update miner.h

--- miner.h
+++ miner.h
@@ -6,8 +6,6 @@ typedef struct {
     // Add other miner-specific data as needed
 } MinerThreadArgs;
 
-void init_miner(int num_miners, int pool_size, int transactions_per_block);
-void start_miner_threads();
-void stop_miner_threads();
+void run_miner_process(int num_threads);
 
 #endif
